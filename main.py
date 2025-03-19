624e3095296fc5bfec71bf675691cdfa809fc69f
bound-one <abhishekanuj356@gmail.com>
2025-03-19 16:09:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import random
 print("welcome")
 print("rules /The computer randomly select a number between 1 and 100.")
 print("you need to guess the number")
-diff = input("select a diffculty 1 = easy 2= mid 3=hard")
+   
+diff = int(input("Select a difficulty: 1 = easy, 2 = mid, 3 = hard: "))
 chance= 0
 if diff ==  1:
   chance = 10
@@ -11,6 +12,10 @@ elif diff == 2:
   chance= 5
 elif diff == 3:
   chance= 3
+else:
+  print("enter a number between 1 and 3")
+  exit()
+print("you have" , chance )
 while chance >= 0 :
   no = random.randint(1, 100)
   guess = int(input("your guess"))
@@ -18,10 +23,13 @@ while chance >= 0 :
     print ("you win")
   elif no > guess:
     print("greater")
-    
   elif no < guess:
-    print("less")  
-  chance = chance - 1  
+    print("less")
+    
+  else:
+    print("enter a number ")  
+  chance = chance - 1
+  
 
 
 
