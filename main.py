a4ae9f498b8d7c72da7c961789c75fc0333bfa3e
bound-one <abhishekanuj356@gmail.com>
2025-03-19 13:51:50 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+print("welcome")
+print("rules The computer randomly select a number between 1 and 100.")
+print("you need to guess the number")
+diff = int(input("select a diffculty "/ "1 = easy" /"2= mid" /"3=hard"))
+if diff =  1:
+  chance = 10
+elif diff = 2:
+  chance= 5  
+else diff = 3:
+  chance= 3
+
+
+  
